c43b9164f5b7b30062950206c6a5d806326aaec9
MAZTRO <designe.mallet@gmail.com>
2019-12-09 18:09:47 -0500

    Fix cuestions

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 	First = list;
 	Second = list;
 
-	while (First->next->next != NULL && Second->next != NULL)
+	while (First->next && Second->next && First)
 	{
 		if (First->next->next == Second->next)
 		{
