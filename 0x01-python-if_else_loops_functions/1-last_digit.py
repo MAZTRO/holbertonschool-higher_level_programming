6961afb506ec520b90f82ee0592f3107565823a9
MAZTRO <designe.mallet@gmail.com>
2019-12-10 14:20:47 -0500

    Fix order in task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,11 +11,11 @@ if number < 0:
  6 and not 0".format(number, mod))
 else:
     mod = number % 10
-    if mod < 6 and mod != 0:
-        print("Last digit of {:d} is {:d} and is less than\
- 6 and not 0".format(number, mod))
-    elif mod > 5:
+    if mod > 5:
         print("Last digit of {:d} is {:d} and is greater\
  than 5".format(number, mod))
     elif mod == 0:
         print("Last digit of {:d} is {:d} and is zero".format(number, mod))
+    elif mod < 6 and mod != 0:
+        print("Last digit of {:d} is {:d} and is less than\
+ 6 and not 0".format(number, mod))
