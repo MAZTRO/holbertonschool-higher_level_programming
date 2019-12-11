90388678773335d1d87c90f658b11cc530949dd0
MAZTRO <designe.mallet@gmail.com>
2019-12-10 21:26:31 -0500

    Fix negative in task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,7 @@ if last > 5:
     print("Last digit of {:d} is {:d} and is greater\
  than 5".format(number, mod))
 elif last == 0:
-    print("Last digit of {:d} is {:d} and is zero".format(number, mod))
+    print("Last digit of {:d} is {:d} and is 0".format(number, mod))
 elif last < 6 and mod != 0:
     print("Last digit of {:d} is {:d} and is less than\
  6 and not 0".format(number, mod))
