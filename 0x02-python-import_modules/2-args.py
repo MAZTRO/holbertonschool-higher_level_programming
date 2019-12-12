12b24d961de0d3f88caf3daeda6d60110f9ef359
MAZTRO <designe.mallet@gmail.com>
2019-12-12 14:02:44 -0500

    Print argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 import sys
-if (sys.argv[1] != NULL):
-    print("{:d} arguments:".format(len(sys.argv)))
+count = 0
+if (len(sys.argv) > 1):
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+    for w in sys.argv[1:]:
+        count += 1
+        print("{:d}: {:s}".format(count, w))
+else:
+    print("{:d} arguments.".format(len(sys.argv) - 1))
