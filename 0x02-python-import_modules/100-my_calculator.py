45cdef5cb510b709005a015df13ce2001565b221
MAZTRO <designe.mallet@gmail.com>
2019-12-12 19:21:39 -0500

    Import all functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,27 @@ if __name__ == "__main__":
     import sys
     import calculator_1
 
+    numb_1 = int(sys.argv[1])
+    opp = sys.argv[2]
+    numb_2 = int(sys.argv[3])
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if (sys.argv[2] == '+'):
-        print("{:d} {:s} {:d} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[1]) + int(sys.argv[3])))
+        print("{:d} {:s} {:d}\
+ = {:d}".format(numb_1, opp, numb_2, numb_1 + numb_2))
         exit(0)
     elif (sys.argv[2] == '-'):
-        print("{:d} {:s} {:d} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[1]) - int(sys.argv[3])))
+        print("{:d} {:s} {:d}\
+ = {:d}".format(numb_1, opp, numb_2, numb_1 - numb_2))
         exit(0)
     elif (sys.argv[2] == '*'):
-        print("{:d} {:s} {:d} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[1]) * int(sys.argv[3])))
+        print("{:d} {:s} {:d}\
+ = {:d}".format(numb_1, opp, numb_2, numb_1 * numb_2))
         exit(0)
     elif (sys.argv[2] == '/'):
-        print("{:d} {:s} {:d} = {:d}".format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), int(sys.argv[1]) // int(sys.argv[3])))
+        print("{:d} {:s} {:d}\
+ = {:d}".format(numb_1, opp, numb_2, numb_1 // numb_2))
         exit(0)
     elif (sys.argv[2] != '+' or sys.argv[2] != '-'):
         print("Unknown operator. Available operators: +, -, * and /")
