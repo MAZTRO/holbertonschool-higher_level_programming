95897fee9c3d98859e879c36c71a03232f025021
MAZTRO <designe.mallet@gmail.com>
2019-12-17 17:57:26 -0500

    True or false

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     if (my_list):
         pos = 0
         for div in my_list:
-            if (pos % 2 == 0):
+            if (div % 2 == 0):
                 my_oun_list.insert(pos, True)
             else:
                 my_oun_list.insert(pos, False)
