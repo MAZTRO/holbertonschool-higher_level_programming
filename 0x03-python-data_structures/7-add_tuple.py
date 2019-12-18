d328a4362e89e322c3b8b0d494c7d241345bcd58
MAZTRO <designe.mallet@gmail.com>
2019-12-17 21:31:43 -0500

    Fix Issues

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (not tuple_a) and (not tuple_b):
         add_0 = 0
         add_1 = 0
+        tuple_c = (add_0, add_1)
+        return (tuple_c)
     if (not tuple_a):
         return (tuple_b)
     if (not tuple_b):
@@ -11,14 +13,20 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
         add_0 = tuple_a[0] + tuple_b[0]
         add_1 = tuple_a[1] + tuple_b[1]
+        tuple_c = (add_0, add_1)
+        return (tuple_c)
     if len(tuple_a) < 2 and len(tuple_b) < 2:
         add_0 = tuple_a[0] + tuple_b[0]
         add_1 = 0
+        tuple_c = (add_0, add_1)
+        return (tuple_c)
     if len(tuple_a) < 2:
         add_0 = tuple_a[0] + tuple_b[0]
         add_1 = tuple_b[1]
+        tuple_c = (add_0, add_1)
+        return (tuple_c)
     if len(tuple_b) < 2:
         add_0 = tuple_a[0] + tuple_b[0]
         add_1 = tuple_a[1]
-    tuple_c = (add_0, add_1)
-    return (tuple_c)
+        tuple_c = (add_0, add_1)
+        return (tuple_c)
