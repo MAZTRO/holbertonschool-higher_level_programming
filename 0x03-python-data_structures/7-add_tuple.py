b2841857041acf770f6ec4e6c25f1d919953988b
MAZTRO <designe.mallet@gmail.com>
2019-12-17 21:23:28 -0500

    Fix Issues

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     add_0, add_1 = 0, 0
-    if (len(tuple_a) == 2 and len(tuple_b) == 2):
+    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
         add_0 = tuple_a[0] + tuple_b[0]
         add_1 = tuple_a[1] + tuple_b[1]
-    if (len(tuple_b) == 1 and len(tuple_a) == 2):
-        add_0 = tuple_a[0] + tuple_b[0]
-        add_1 = tuple_a[1] + 0
-    if (len(tuple_a) == 1 and len(tuple_b) == 2):
-        add_0 = tuple_a[0] + tuple_b[0]
-        add_1 = tuple_b[1] + 0
-    if (len(tuple_b) == 0 and len(tuple_a) == 2):
-        add_0 = tuple_a[0] + 0
-        add_1 = tuple_a[1] + 0
-    if (len(tuple_a) == 0 and len(tuple_b) == 2):
-        add_0 = tuple_b[0] + 0
-        add_1 = tuple_b[1] + 0
     if (len(tuple_a) == 0 and len(tuple_b) == 0):
-        add_0 = 0 + 0
-        add_1 = 0 + 0
-
+        add_0 = 0
+        add_1 = 0
+    if (not tuple_a):
+        return (tuple_b)
+    if (not tuple_b):
+        return (tuple_a)
+    if len(tuple_a) < 2 and len(tuple_b) < 2:
+        add_0 = tuple_a[0] + tuple_b[0]
+        add_1 = 0
+    if len(tuple_a) < 2:
+        add_0 = tuple_a[0] + tuple_b[0]
+        add_1 = tuple_b[1]
+    if len(tuple_b) < 2:
+        add_0 = tuple_a[0] + tuple_b[0]
+        add_1 = tuple_a[1]
     tuple_c = (add_0, add_1)
     return (tuple_c)
