fa1c18fc1769d63bb01def10c2468786cafa1406
MAZTRO <designe.mallet@gmail.com>
2019-12-19 12:49:16 -0500

    Fix it

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@ def search_replace(my_list, search, replace):
     if (my_list):
         N = my_list.copy()
         N = list(map(lambda A: A if A != search else replace, N))
-        return oun_list
+        return N
