d42fad3f8820d10c477ac6a513e4fe15b3689759
MAZTRO <designe.mallet@gmail.com>
2019-12-19 12:50:21 -0500

    Fix it

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (my_list):
-        N = my_list.copy()
-        N = list(map(lambda A: A if A != search else replace, N))
-        return N
+    N = my_list.copy()
+    N = list(map(lambda A: A if A != search else replace, N))
+    return N
