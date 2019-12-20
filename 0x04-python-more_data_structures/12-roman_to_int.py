15eda34718a3ffd26253815acd70e8ddd9f16c9b
MAZTRO <designe.mallet@gmail.com>
2019-12-19 19:01:48 -0500

    Roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (type(roman_string) is str or roman_string is not None):
+    if (type(roman_string) is not str or roman_string is None):
+        return (0)
+    else:
         D = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         X = 0
         if (len(roman_string) >= 2):
@@ -17,5 +19,3 @@ def roman_to_int(roman_string):
             for n in roman_string:
                 X += sum(map(lambda R: D[R] if R == n else 0, D))
             return (X)
-    else:
-        return (0)
