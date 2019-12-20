5e63c1f901df3f6b1ea9b0ade6b440c585fdbc52
MAZTRO <designe.mallet@gmail.com>
2019-12-19 20:07:58 -0500

    todo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,12 +5,23 @@ def roman_to_int(roman_string):
     else:
         D = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         X = 0
+        Y = 0
         if (len(roman_string) >= 2):
             if (roman_string[-2] < roman_string[-1]):
                 for n in roman_string:
                     X += sum(map(lambda R: D[R] if R == n else 0, D))
-                X = X - D[n] - D[n]
-                return (-X)
+                if (len(roman_string) > 2):
+                    for a in range(len(roman_string) - 1, 1, -1):
+                        print(D[roman_string[a]])
+                        if (roman_string[a - 1] < roman_string[a]):
+                            Y += D[roman_string[a - 1]]
+                    X = X - (Y * 2)
+                else:
+                    X -= D[roman_string[-2]] * 2
+                if (X < 0):
+                    return (-X)
+                else:
+                    return (X)
             else:
                 for n in roman_string:
                     X += sum(map(lambda R: D[R] if R == n else 0, D))
