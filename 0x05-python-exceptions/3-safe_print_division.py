88a0a311c7a453a8a19923a1f964da8c9cc8cb93
MAZTRO <design.mallet@gmail.com>
2020-01-08 14:07:13 -0500

    Print the divition

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    res = 0
     try:
         res = a // b
         print("Inside result: {:d}".format(res))
-        return (res)
     except ZeroDivisionError:
         print("Inside result: {}".format(None))
         return (None)
+    finally:
+        return (res)
