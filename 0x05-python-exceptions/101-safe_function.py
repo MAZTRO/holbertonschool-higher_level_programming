18be7dcc63f712da49887388ab74aa0fa6e83d5f
MAZTRO <designe.mallet@gmail.com>
2020-01-08 19:35:00 -0500

    Prit safetly function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         res = 0
-        res = fct(args[0], args[1])
+        res = fct(*args)
         return (res)
     except (TypeError, ValueError, ZeroDivisionError, IndexError) as stderror:
         sys.stderr.write("Exception: {}\n".format(stderror))
