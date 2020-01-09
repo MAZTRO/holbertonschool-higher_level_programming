956f9028e301e1cfdf4f48101f562e052514e3e1
MAZTRO <designe.mallet@gmail.com>
2020-01-09 18:11:33 -0500

    Safe propierty

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,4 +19,21 @@ class Square:
             self.__size = size
 
     def area(self):
-        return (self.__size ** 2)
+        """ area method to power the atributes.
+        Args:
+            Nothing.
+        """
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
