aa8ce9f13493eb4c06e13df0373dd3ef93bcf208
MAZTRO <designe.mallet@gmail.com>
2020-01-21 17:15:36 -0500

    Fix outputs

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,14 +5,13 @@ load_js_file = __import__('8-load_from_json_file').load_from_json_file
 save_js_file = __import__('7-save_to_json_file').save_to_json_file
 
 try:
-    my_list = []
     my_list = load_js_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+finally:
     count = 0
     for av in sys.argv:
         if (count >= 1):
             my_list.append(av)
         count += 1
-except FileNotFoundError:
-    pass
-finally:
     save_js_file(my_list, "add_item.json")
