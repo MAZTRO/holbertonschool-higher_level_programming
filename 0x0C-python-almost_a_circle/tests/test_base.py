03ec7c8567d98d79c00d40f913d0e33162d137e4
MAZTRO <designe.mallet@gmail.com>
2020-01-27 17:37:41 -0500

    test Cases

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -98,19 +98,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(S2.__str__(), "[Square] (2) 4/0 - 3")
         self.assertEqual(S3.__str__(), "[Square] (3) 4/5 - 3")
 
-# =======END TASK 10 =======
-
-# ======= TASK 17 =========
-
-    def test_from_json_string(self):
-        string_js = '[{"id": 89, "width": 10, "height": 4, "x": 1, "y": 2}, \
-{"id": 7, "width": 1, "height": 7, "x": 6, "y": 5}]'
-        jsonconv = Base.from_json_string(string_js)
-        self.assertTrue(type(jsonconv) is list)
-        self.assertEqual(len(jsonconv), 2)
-
-# ====== END TASK 17 ======
-
 
 if __name__ == "_main_":
     unittest.main()
