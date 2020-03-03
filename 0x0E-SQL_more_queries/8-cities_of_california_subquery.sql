6250a09dcfb0f2a8677b185f221066c077362cdc
MAZTRO <design.mallet@gmail.com>
2020-03-03 19:11:35 +0000

    Add task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- script that lists all the cities of California that can be found in the database hbtn_0d_usa
+-- Find a specific state ID
+SELECT id, name FROM cities a WHERE EXISTS (SELECT * FROM states b WHERE a.state_id = b.id)
