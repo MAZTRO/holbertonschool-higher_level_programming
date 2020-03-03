888a8c5058ecff1c65392c563164add68a9e857e
MAZTRO <design.mallet@gmail.com>
2020-03-03 18:14:17 +0000

    add task 2 and fix task 0

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- Create a Database and a second User --
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON `hbtn_0d_2`. * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
