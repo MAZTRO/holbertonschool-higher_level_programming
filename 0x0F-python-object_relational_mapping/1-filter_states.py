252520a375653c4c6cd7cef34c489f7beaf180e1
MAZTRO <design.mallet@gmail.com>
2020-03-28 17:10:17 +0000

    Fix pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,16 +9,16 @@ if __name__ == '__main__':
 ", user=av[1], passwd=av[2], db=av[3], port=3306)
     cur = db.cursor()
 
-    """ cur.execute("SELECT * FROM states WHERE name LIKE \
+    cur.execute("SELECT * FROM states WHERE name LIKE \
 'N%' ORDER BY states.id ASC")
     for row in cur.fetchall():
-        print(row) """
+        print(row)
 
-    cur.execute("SELECT * FROM states")
+    """ cur.execute("SELECT * FROM states")
     fil = ["N", "n"]
     for row in cur.fetchall():
         if (row[1][0] in fil):
-            print(row)
+            print(row) """
 
     db.close()
     cur.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 ", user=av[1], passwd=av[2], db=av[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY \
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY \
 states.id ASC".format(av[4]))
     for row in cur.fetchall():
         print(row)
