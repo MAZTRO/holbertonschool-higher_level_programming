4d5e5f71d12bfd5e3675223d1de8970347b99c24
MAZTRO <design.mallet@gmail.com>
2020-03-28 17:30:01 +0000

    Fix sintax in task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
 ", user=av[1], passwd=av[2], db=av[3], port=3306)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE states.name LIKE \
-'N%' ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states WHERE states.name LIKE \
+BINARY 'N%' ORDER BY states.id ASC""")
     for row in cur.fetchall():
         print(row)
 
