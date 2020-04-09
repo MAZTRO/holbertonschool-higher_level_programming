730297206338db47bab51b8780279b6ce14cee1e
MAZTRO <design.mallet@gmail.com>
2020-04-09 17:27:43 +0000

    Fix pep8

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | awk '/Allow:/ {print $2 " " $3 " " $4}' # curl -sI "$1" | awk '/Allow:/ {for(x=2;x<=NF;++x){printf "%s", $x}; printf "\n"}' // curl -sI "$1" | awk '/Allow:/ {$1=""; print}'
+curl -sI "$1" | awk '/Allow:/ {print $2 " " $3 " " $4}' # curl -sI $1 | grep Allow: | cut -d " " -f 2-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,6 +9,5 @@ def find_peak(list_of_integers):
     if (list_of_integers):
         for idx in range(1, len(ls) - 1):
             if (ls[idx] >= ls[idx - 1] and ls[idx] >= ls[idx + 1]):
-                if 
                 peak = ls[idx]
     return (peak)
