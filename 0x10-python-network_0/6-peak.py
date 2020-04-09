41ab2b708bebeb587f5b987148ff9d61cf04b753
MAZTRO <design.mallet@gmail.com>
2020-04-09 18:13:48 +0000

    First conditional added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,8 @@ def find_peak(list_of_integers):
     ls = list_of_integers
     peak = None
 
+    if len(list_of_integers) == 0:
+        return None
     if (list_of_integers):
         for idx in range(1, len(ls) - 1):
             if (ls[idx] >= ls[idx - 1] and ls[idx] >= ls[idx + 1]):
