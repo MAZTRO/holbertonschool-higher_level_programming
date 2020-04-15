134c720a3fd96d2714b3b18719ce4368c15c6d22
MAZTRO <design.mallet@gmail.com>
2020-04-15 17:32:04 +0000

    Change to .format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         res = response.read()
     print("Body response:")
-    print("\t- type: ", type(res))
-    print("\t- content: ", res)
-    print("\t- utf8 content: ", res.decode())
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode()))
