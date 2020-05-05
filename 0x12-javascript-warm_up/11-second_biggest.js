b77593168cc7151fc87e4dc5db2668302a2d8965
MAZTRO <design.mallet@gmail.com>
2020-05-05 18:33:40 +0000

    Fix output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,5 @@ if (myArray.length === 0 || myArray.length === 1) {
   console.log(0);
 } else {
   myArray.sort(function (a, b) { return b - a; });
+  console.log(parseInt(myArray[1]));
 }
-console.log(parseInt(myArray[1]));
