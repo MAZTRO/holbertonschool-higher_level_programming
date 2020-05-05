989f836083daa89d10df61aa642815193770495b
MAZTRO <design.mallet@gmail.com>
2020-05-05 17:43:04 +0000

    Add conditional if we can parse to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length > 2) {
+if (process.argv[2] && process.argv[2] % 1 === 0) {
   console.log(parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
