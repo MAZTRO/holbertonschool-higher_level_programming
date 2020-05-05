c84999ec87c738d7c234975af995b6de778b9175
MAZTRO <design.mallet@gmail.com>
2020-05-05 17:52:20 +0000

    Change variable name

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2])) {
-  console.log(parseInt(process.argv[2]));
+const myVar = process.argv;
+
+if (parseInt(myVar[2])) {
+  console.log(`My number: ${parseInt(myVar[2])}`);
 } else {
   console.log('Not a number');
 }
