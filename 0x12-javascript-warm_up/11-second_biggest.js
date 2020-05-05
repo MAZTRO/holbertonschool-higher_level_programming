4e5bb3ec065d48b16f241ca460dd32d26123d4b7
MAZTRO <design.mallet@gmail.com>
2020-05-05 18:30:58 +0000

    Add task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const myArray = process.argv;
+myArray.splice(0, 2);
+if (myArray.length === 0 || myArray.length === 1) {
+  console.log(0);
+} else {
+  myArray.sort(function (a, b) { return b - a; });
+}
+console.log(parseInt(myArray[1]));
