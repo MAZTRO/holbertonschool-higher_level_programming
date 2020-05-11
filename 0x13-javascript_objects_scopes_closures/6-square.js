8d586586862755028b2b5cd4d6c71ee494006439
MAZTRO <design.mallet@gmail.com>
2020-05-11 18:25:16 +0000

    Add task 5 and 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let character = 'C';
+
+    if (!c) {
+      character = 'X';
+    }
+    let x = 0;
+    while (x < this.height) {
+      console.log(`${character}`.repeat(this.width));
+      x++;
+    }
+  }
+};
