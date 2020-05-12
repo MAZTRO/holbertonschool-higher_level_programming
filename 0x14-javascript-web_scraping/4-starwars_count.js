14f56a86c66f682798127371312af542014ded1c
MAZTRO <design.mallet@gmail.com>
2020-05-12 18:13:05 +0000

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+
+request(args[0], (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  }
+  if (response.statusCode === 200) {
+    let count = 0;
+    let movieList = JSON.parse(body).results;
+
+    for (let idx = 0; idx < movieList.length; idx++) {
+      let characters = movieList[idx].characters;
+      if (characters.indexOf('https://swapi-api.hbtn.io/api/people/18/') !== -1) {
+        count -= -1;
+      }
+    }
+    console.log(count);
+  }
+});
