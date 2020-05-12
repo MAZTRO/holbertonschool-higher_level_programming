7915154a51ca0f3994f737ebb92fd793f16fe7bf
MAZTRO <design.mallet@gmail.com>
2020-05-12 21:19:39 +0000

    Change all conditionals

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,23 +3,22 @@ const args = process.argv.slice(2);
 const request = require('request');
 
 request(args[0], (error, response, body) => {
-  if (error) console.error('error:', error);
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
 
   if (response.statusCode === 200) {
-    let countTrue = 0;
-    let countFalse = 0;
     const TodoList = JSON.parse(body);
     const final = {};
 
     for (let idx = 0; idx < TodoList.length; idx++) {
       if (TodoList[idx].completed === true) {
-        countTrue -= -1;
-      } else {
-        countFalse -= -1;
-      }
-      final[TodoList[idx].userId] = countTrue;
-      if (countTrue + countFalse === 20) {
-        countTrue = countFalse = 0;
+        if (final[TodoList[idx].userId]) {
+          final[TodoList[idx].userId] = final[TodoList[idx].userId] + 1;
+        } else {
+          final[TodoList[idx].userId] = 1;
+        }
       }
     }
     console.log(final);
